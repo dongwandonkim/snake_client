3df7f03a9a37c94d1e727d8ac3e1f0572e8abbc5
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-11 17:01:13 -0700

    key

--- input.js
+++ input.js
@@ -11,28 +11,32 @@ const setupInput = function (conn) {
   return stdin;
 };
 
+const msg = (key) => {
+  const txt = ['hi', 'bye', 'yum yum'];
+
+  if (key === 't') {
+    connection.write(txt[0]);
+  }
+  if (key === 'u') {
+    connection.write(txt[1]);
+  }
+  if (key === 'i') {
+    connection.write(txt[2]);
+  }
+};
+
 const handleUserInput = function (key) {
   // your code here
-  switch (key) {
-    case 'w':
-      connection.write('Move: up');
-      break;
-    case 's':
-      connection.write('Move: down');
-      break;
-    case 'a':
-      connection.write('Move: left');
-      break;
+  msg(key);
+  if (key === 'w') connection.write('Move: up');
+  if (key === 's') connection.write('Move: down');
+  if (key === 'a') connection.write('Move: left');
+  if (key === 'd') connection.write('Move: right');
 
-    case 'd':
-      connection.write('Move: right');
-      break;
-    default:
-      console.log('default');
-  }
   if (key === '\u0003') {
     console.log('bye');
     process.exit();
   }
 };
+
 module.exports = {setupInput};
